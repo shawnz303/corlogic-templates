163013f2a51fa2549ea9fb779e0b14d6d8edb410
jverce <jay.vercellone@toptal.com>
2017-11-24 02:31:48 -0300

    11 create components for file upload and preview

---
+++ js/components/TransmissionUploader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label class="btn--select">
+            <div class="btn btn--blue">
+                <span v-if="value">Selected File: {{value.name}}</span>
+                <span v-else>+ Add Session Manually</span>
+            </div>
+            <input type="file" @change="handleFileChange"/>
+        </label>
+        <transmission-preview v-if="fileOpened"></transmission-preview>
+    </div>
+</template>
+
+<script>
+    import TransmissionPreview from './TransmissionPreview.vue';
+
+    export default {
+        data: () => ({
+            fileOpened: false,
+        }),
+        components: {
+            TransmissionPreview,
+        },
+        methods: {
+            handleFileChange(e) {
+                this.fileOpened = true;
+                this.$emit('input', e.target.files[0]);
+            },
+        },
+    };
+</script>
