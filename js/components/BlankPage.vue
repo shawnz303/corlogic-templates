ac8e90e2da810da19119a43c421f57fb2cc96257
shawnz303 <shawnz303@yahoo.com>
2018-02-27 16:58:55 -0800

    add content to Need Help link

--- js/components/BlankPage.vue
+++ js/components/BlankPage.vue
@@ -4,6 +4,35 @@
 
         <div class="content content--patient">
             <div class="content__body">
+<h1>Frequently Asked Questions</h1>
+        <h2>How do I add a new user to Corlogic?</h2>
+
+    <p>
+            If you have Staff priviledges, go to the Admin section as seen below:   
+            <img src='admin-settings section.png' height='300' width='1000'><br>
+	   <br>
+        
+        Then click on Users, and use the Green plus sign to add a new user!
+    </p>
+        
+    <h2>How do I filter my dashboard to see Urgent Transmissions only?</h2>
+    <p>
+	Click on the Urgent Transmissions Icon on the Dashboard. This filter will show patients with an ERI alert or with shocks.
+            <img src='filter-urgent.png'>
+	</p>
+	
+    <p>
+    <h2>How do I search for a particular serial number or patient name?</h2>
+	<img src='search.png'>
+    </p>
+    <h2>How do I stop receiving HF transmissions for a particular patient?</h2>
+    
+    <p>    	
+    If the patient is an Abbott/St. Jude patient, remove the patient from the HF Portal inside merlin.net.  If the patient has a Medtronic device, remove the patient from your stand-alone Medtronic HF portal.
+    </p>
+
+
+
             </div><!-- /.content__body -->
         </div><!-- /.content -->
     </div><!-- main -->
