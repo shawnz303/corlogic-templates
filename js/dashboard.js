27cf27dd79ec879d4fcbd78e21ebfc67d17b6e28
xoho <eric.hoxworthy@gmail.com>
2018-03-06 15:32:06 -0600

    Correct url for bilings update

--- js/dashboard.js
+++ js/dashboard.js
@@ -141,7 +141,7 @@ window.onload = () => {
                 });
             },
             bill({ commit, state }, id){
-                const url = `/api/v1/billings/${id}/`;
+                const url = `/api/v1/reports/bilings/${id}/`;
                 const body = {billed: true};
                 return Vue.http.patch(url, body).then(res => {
                     commit('remove', id);
