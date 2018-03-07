46bcc4cd62aa1ef6570be11918298780fc5ae8f6
Shawn Kumar <shawnz303@yahoo.com>
2018-03-07 14:39:22 -0800

    Update BlankPage.vue

--- js/components/BlankPage.vue
+++ js/components/BlankPage.vue
@@ -9,6 +9,7 @@
         <h2><a href='#urgent'>How do I filter my dashboard to see Urgent Transmissions only?</a></h2>
         <h2><a href='#serial'>How do I search for a particular serial number or patient name?</a></h2>
         <h2><a href='#hf'>How do I stop receiving HF transmissions for a particular patient?</a></h2>
+	<h2><a href='#changeout'>How do I register a patient device changeout?</a></h2>
 
         <h2 id='new'>How do I add a new user to Corlogic?</h2>
 
@@ -35,6 +36,10 @@
     <p>    	
     If the patient is an Abbott/St. Jude patient, remove the patient from the HF Portal inside merlin.net.  If the patient has a Medtronic device, remove the patient from your stand-alone Medtronic HF portal.
     </p>
+    <p>
+    <h2 id='changeout'>How do I register a patient device changeout?</h2>
+    <p>
+    Fill out the information in this <a href="https://docs.google.com/forms/d/e/1FAIpQLScImY9530aLuAoUtDbz_Dq-2LdqlO3xnEIZ1d0A37hLwY-mlA/viewform?usp=sf_link">form</a> and the patient's device changeout will be registered in 24 hours.
 
 
 
